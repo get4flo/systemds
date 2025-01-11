64b95e45e96105ec3855e945176fe349b6847e68
Melisa Akbaydar <melisa16@tu-berlin.de>
2025-01-11 19:20:58 +0100

    update sqrt matrix test files in java and dml

--- src/test/java/org/apache/sysds/test/functions/builtin/part2/BuiltinSQRTMatrixTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/part2/BuiltinSQRTMatrixTest.java
@@ -41,24 +41,31 @@ public class BuiltinSQRTMatrixTest extends AutomatedTestBase {
 		addTestConfiguration(TEST_NAME, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[] {"C"}));
 	}
 
+	// tests for strategy "COMMON"
 	@Test
 	public void testSQRTMatrix() {
 		runSQRTMatrix(true, ExecType.CP, "COMMON", 1);
 	}
+
+	/* TODO: add more test cases for COMMON and then for other strategies
+	@Test
+	public void testSQRTMatrix() {
+
+	}
+	 */
 
 	private void runSQRTMatrix(boolean defaultProb, ExecType instType, String strategy, int test_case) {
 		Types.ExecMode platformOld = setExecMode(instType);
 		try {
 			loadTestConfiguration(getTestConfiguration(TEST_NAME));
 
+			// find path to associated dml script and define parameters
 			String HOME = SCRIPT_DIR + TEST_DIR;
 			fullDMLScriptName = HOME + TEST_NAME + ".dml";
-			programArgs = new String[] {"-args", input("X"), strategy, output("Y")};
+			programArgs = new String[] {"-args", input("X"), input("S"), output("Y"), output("R")};
 
-			//data and model consistent with decision tree test
+			// define input matrix for the matrix sqrt function according to test case
 			double[][] X = null;
-			double[][] M = null;
-
 			switch(test_case) {
 				case 1:
 					double[][] X1 = {
@@ -69,19 +76,44 @@ public class BuiltinSQRTMatrixTest extends AutomatedTestBase {
 							{2, 2, 1, 5, 1},};
 					X = X1;
 					break;
+				case 2:
+					double[][] X2 = { // TODO: change values
+							{3, 1, 2, 1, 5},
+							{2, 1, 2, 2, 4},
+							{1, 1, 1, 3, 3},
+							{4, 2, 1, 4, 2},
+							{2, 2, 1, 5, 1},};
+					X = X2;
+					break;
+					// TODO: add more test cases
 			}
 
-			writeInputMatrixWithMTD("X", X, true);
+			assert X != null;
 
+			// write the input matrix and strategy for matrix sqrt function to dml script
+            writeInputMatrixWithMTD("X", X, true);
+			writeInputMatrixWithMTD("S", strategy, true); // TODO: find how to write a string to dml
+
+			// run the test dml script
 			runTest(true, false, null, -1);
 
-			HashMap<MatrixValue.CellIndex, Double> accuracy = readDMLScalarFromOutputDir("Y");
+			// read the result matrix from the dml script output Y
+			HashMap<MatrixValue.CellIndex, Double> actual_Y = readDMLMatrixFromOutputDir("Y");
+
+			// create a HashMap with Matrix Values from the input matrix X to compare to the received output matrix
+			HashMap<MatrixValue.CellIndex, Double> expected_Y = new HashMap<>();
+			for (int r = 0; r < X.length; r++) {
+				for (int c = 0; c < X[0].length; c++) {
+					expected_Y.put(new MatrixValue.CellIndex(r, c), X[r][c]);
+				}
+			}
+
+			// compare the expected matrix (the input matrix X) with the received output matrix Y, which should be the (SQRT_MATRIX(X))^2 = X again
+			TestUtils.compareMatrices(expected_Y, actual_Y, eps, "Expected-DML", "Actual-DML");
+
+			// read the result accuracy from the dml script output R
+			HashMap<MatrixValue.CellIndex, Double> accuracy = readDMLScalarFromOutputDir("R");
 			System.out.println(accuracy.toString());
-			//HashMap<MatrixValue.CellIndex, Double> actual_Y = readDMLMatrixFromOutputDir("Y");
-
-
-
-			//TestUtils.compareMatrices(expected_Y, actual_Y, eps, "Expected-DML", "Actual-DML");
 		}
 		finally {
 			resetExecMode(platformOld);
